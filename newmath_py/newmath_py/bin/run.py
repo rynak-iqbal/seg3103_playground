ba8a3a9f17690ca6134b73a6e9bc3adb170667d0
rynak-iqbal <115375081+rynak-iqbal@users.noreply.github.com>
2024-05-09 22:21:45 -0400

    Delete newmath_py/newmath_py directory

--- newmath_py/newmath_py/bin/run.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-import code
-import newmath
--- newmath_py/newmath_py/src/newmath.py
+++
@@ -1,5 +0,0 @@
-def div(number1, number2):
-    if number2 == 0:
-        raise ZeroDivisionError("Division by zero is not allowed.")
-    else:
-        return number1 / number2
