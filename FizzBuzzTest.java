e4932a282d154d9e6cdb0ef77385b49bdc022afe
rynak-iqbal <115375081+rynak-iqbal@users.noreply.github.com>
2024-06-06 18:34:41 -0400

    Add files via upload

---
+++ FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+	public void returnsOneIfGivenOne() {
+  Fizzbuzz fizzbuzz = new Fizzbuzz();
+  String result = fizzbuzz.getResult(1);
+  assertEquals("1", result);
+}
+}
