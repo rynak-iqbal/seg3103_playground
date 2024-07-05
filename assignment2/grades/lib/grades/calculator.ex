9406f4d79f429a4791ba8587635d96c8ddf548be
Rynak Iqbal <rynak@rynaks-laptop.home>
2024-07-04 20:04:06 -0400

    extract helper method calculate_grade

--- assignment2/grades/lib/grades/calculator.ex
+++ assignment2/grades/lib/grades/calculator.ex
@@ -11,11 +11,15 @@ defmodule Grades.Calculator do
     avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
   end
 
+  defp calculate_grade(avg_labs, avg_homework, midterm, final) do
+    0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+  end
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)
     avg_labs = avg(labs)
 
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
     round(mark * 100)
   end
 
@@ -28,7 +32,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> "A+"
@@ -55,7 +59,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> 10
